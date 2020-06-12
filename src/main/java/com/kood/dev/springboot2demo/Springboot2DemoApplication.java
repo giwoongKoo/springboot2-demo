1ae3598e0ed9d3332ce7118670e2344f1efc3145
gwkoo <gwkoo@hellonature.net>
2020-06-12 10:47:53 +0900

    init project

---
+++ src/main/java/com/kood/dev/springboot2demo/Springboot2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.kood.dev.springboot2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kood/dev/springboot2demo/Springboot2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kood.dev.springboot2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
