a0ec5e8bed72b5f6267141a845ff96c359dd1b18
gwkoo <gwkoo@hellonature.net>
2020-06-15 11:53:02 +0900

    머스테치로 화면 구성하기

--- src/main/java/com/kood/dev/springboot2demo/web/IndexController.java
+++ src/main/java/com/kood/dev/springboot2demo/web/IndexController.java
@@ -2,9 +2,8 @@ package com.kood.dev.springboot2demo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
