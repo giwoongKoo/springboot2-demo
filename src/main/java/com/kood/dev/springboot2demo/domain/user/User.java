00156fbbdcae858ea1e3d3d8e5586915f5cfe4fe
gwkoo <gwkoo@hellonature.net>
2020-06-16 11:03:57 +0900

    구글 로그인 연동

---
+++ src/main/java/com/kood/dev/springboot2demo/domain/user/User.java
@@ -0,0 +1,48 @@
+package com.kood.dev.springboot2demo.domain.user;
+
+import com.kood.dev.springboot2demo.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
